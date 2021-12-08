bdfd496b26124ffef5313f4485133b9aa6080cc4
Tony Huang <tonyhuang@Tonys-MacBook-Pro.local>
2021-12-07 19:42:37 -0500

    roughly done

--- search.js
+++ search.js
@@ -5,20 +5,38 @@ const MongoClient = require('mongodb').MongoClient;
 const url = "mongodb+srv://tonyhuang12138:Tony123456@cluster0.ypk43.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
 const port = process.env.PORT || 3000;
 
-// app.use(bodyParser.urlencoded({ 
-//     extended: true })
-// );
+app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get('/search', function (req, res) {
-    res.sendFile(__dirname + '/index.html')
+app.get('/', function (req, res) {
+    res.sendFile(__dirname + '/index.html');
 })
 
-app.post('/search', function (req, res) {
-    console.log("HI");
-    var ticker = String(req.body.type);
-    var company = String(req.body.company);
+// Establish connection
+MongoClient.connect(url, { useUnifiedTopology: true }, function (err, db) {
+
+    if (err) { return console.log(err); }
+    // ASYNC FUNCTION?
+    var dbo = db.db("database");
+    var collection = dbo.collection('companies');
+
+    app.post('/', async (req, res) => {
+        var typeResult = String(req.body.type);
+        var searchResult = String(req.body.search);
+        if (typeResult == 'ticker') {
+            const result = await collection.findOne({ Company: searchResult });
+            res.send(result.Company + " : " + result.Ticker);
+        } else { // == 'company'
+            const result = await collection.find({ Ticker: searchResult }).toArray();
+            // console.log(result);
+            res.writeHead(200, { "Content-Type": "text/html" });
+            for (i = 0; i < result.length; i++) {
+                res.write(result[i].Company + " : " + result[i].Ticker + "<br>");
+            }
+        }
+    });
+
 });
 
 app.listen(port, function () {
-    console.log('listening on port')
+    console.log('listening on port');
 })
