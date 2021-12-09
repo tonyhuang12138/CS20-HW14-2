fe1e999ebf53177acc979d6bea3e510ea483b0e2
Tony Huang <tonyhuang@Tonys-MacBook-Pro.local>
2021-12-08 20:38:58 -0500

    fix heroku support

--- index.js
+++ index.js
@@ -8,14 +8,15 @@ const fs = require("fs");
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get('', (req, res) => {
+// app.get('', (req, res) => {
+    
+// });
+
+app.get('/', function (req, res) {
     fs.readFile('index.html', function (err, txt) {
         res.writeHead(200, {'Content-Type': 'text/html'});
         res.write(txt);
     });
-});
-
-app.get('/', function (req, res) {
     res.sendFile(__dirname + '/index.html');
 })
 app.post('/', function (req, res) {
